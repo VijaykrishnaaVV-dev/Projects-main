bbe8bfa0e6792b440ff8f8f39d053f659a9bdd54
VijaykrishnaaVV-dev <vijaykrishnaa.v.v@gmail.com>
2025-06-28 14:11:35 +0530

    Normalize all line endings to LF

--- Porfolio-web/craco.config.js
+++ Porfolio-web/craco.config.js
@@ -1,4 +1,15 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
+  webpack: {
+    plugins: [
+      new ESLintPlugin({
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        emitWarning: true,
+        failOnError: false,
+      }),
+    ],
+  },
   style: {
     postcssOptions: {
       plugins: [
